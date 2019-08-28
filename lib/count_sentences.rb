7bf500c2245ccf3477fdcca28cd69c3925595ea7
Tom White <tomjameswhite94@gmail.com>
2019-08-28 19:20:28 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else false
   end
+end
 
   def question?
-
-  end
+    if self.end_with?("?")
+      return true
+    else false
+  end 
+end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else false
   end
+end
 
   def count_sentences
-
-  end
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+    #I know how count_sentences works, but I would not have been able finish it on my own...
+     end
 end
