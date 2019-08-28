5d0ebdf9b3f512a656d24da0525e79d38ee6bf0e
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-08-28 14:38:09 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,35 @@
-require 'pry'
+
 
 class String
 
+
+  attr_accessor :string
+
   def sentence?
-    
+    if self.end_with?(".") 
+      return true
+    else 
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?") 
+      return true
+    else 
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") 
+      return true
+    else 
+      return false
+    end
   end
 
   def count_sentences
-
+    scan(/[.!?]+(?=\s|\z)/).size #using a positive lookahead character (?=\s|\z)/
   end
 end
